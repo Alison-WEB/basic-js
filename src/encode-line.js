954734fb014d8b88aede19260afdc51b384c0d2c
Alison WEB <AlisonWEB@yandex.ru>
2023-12-26 02:47:00 +0300

    feat: 23 tests passing

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let array = [];
+  for (let i = 0, count = 1; i < str.length; i += 1) {
+    while (str[i] === str[i + 1]) {
+      count += 1;
+      i += 1;
+    }
+    if (count === 1) {
+      array.push('', str[i])
+    } else {
+      array.push(count, str[i]);
+      count = 1;
+    }
+  }
+  return array.join('');
 }
 
 module.exports = {
--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  for (let i = 0; i < names.length; i += 1) {
+    let count = 1;
+    let remainingNames = names.slice(i + 1);
+    remainingNames.forEach((elem, index) => {
+      if (elem === names[i]) {
+        names[index + i + 1] += `(${count})`;
+        count += 1;
+      }
+    })
+  }
+  return names;
 }
 
 module.exports = {
